559154b9d64b872c500dc9d03fd6d3f8ccf8dbdf
Andrii Sereda <a.a.sereda@ukr.net>
2023-03-16 09:27:37 +0200

    ready hw

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(express.json());
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter, filesRouter);
-// app.use("/api/users", filesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
--- controller/filesController.js
+++ controller/filesController.js
@@ -1,11 +1,11 @@
 const Jimp = require("jimp");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
+const { patchAvatar } = require("../service/usersService");
 
 const FILE_DIR = path.resolve("./public/avatars");
 
-const parseAvatar = async (path) => {
-  const filePathName = `${FILE_DIR}${uuidv4()}.jpg`;
+const convertAvatar = async (path, filePathName) => {
   return Jimp.read(path)
     .then((image) => {
       image.resize(250, 250).quality(60).write(filePathName);
@@ -16,12 +16,18 @@ const parseAvatar = async (path) => {
 };
 
 const filesUploadController = async (req, res, next) => {
+  const filePathName = path.join(FILE_DIR, `${uuidv4()}.jpg`);
+  convertAvatar(req.file.path, filePathName);
+  const { _id } = req.user;
   try {
-    return res.status(200).json({
-      code: 200,
-      message: "succes",
-      avatarURL: await parseAvatar(req.file.path),
-    });
+    const data = await patchAvatar(_id, filePathName);
+    if (data) {
+      return res.status(200).json({
+        code: 200,
+        message: "succes",
+        avatarURL: filePathName,
+      });
+    }
   } catch (e) {
     res.json({ message: e });
     next(e);
