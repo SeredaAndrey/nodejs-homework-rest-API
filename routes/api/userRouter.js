4a9e1b87121b300cfa7dcbc5cf42de29aee8491a
Andrii Sereda <a.a.sereda@ukr.net>
2023-03-22 22:45:44 +0200

    ready hw

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const {
   patchSubscriptionController,
+  verificationController,
 } = require("../../controller/usersController");
 const { asyncWrapper } = require("../../errorHandler/errors");
 const { authMaiddleware } = require("../../middleware/authMiddleware");
 
 const router = express.Router();
+
+router.get("/verify/:verificationToken", asyncWrapper(verificationController));
 
 router.use(authMaiddleware);
 
--- service/usersService.js
+++ service/usersService.js
@@ -16,7 +16,23 @@ const patchAvatar = async (_id, filePathName) => {
   );
 };
 
+const verification = async (email) => {
+  const data = await User.findOne({
+    email,
+    verify: false,
+  });
+  if (!data) {
+    return;
+  }
+  return await User.findByIdAndUpdate(
+    { _id: data._id },
+    { verificationToken: null, verify: true },
+    { new: true }
+  );
+};
+
 module.exports = {
   patchSubscription,
   patchAvatar,
+  verification,
 };
